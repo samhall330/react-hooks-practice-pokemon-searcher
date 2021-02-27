7f9acd1ada611b887f3075d770b9f026f9f6623f
Sam Hall <mann.samantha11@gmail.com>
2021-02-27 18:16:33 -0500

    commit tree 1895be2bd44b8e915e52f06bd4862c8b2e23a1b7

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,16 @@ import PokemonPage from "./PokemonPage";
 
 function App() {
   const [pokemon, setPokemon] = useState([])
-  const [searchQuery, setSearchQuery] = useState("")
-
   useEffect(() => {
     fetch("http://localhost:3001/pokemon")
     .then(response => response.json())
-    .then(pokeData => setPokemon(pokeData.filter((poke) => poke.name.includes(searchQuery))))
-  }, [searchQuery])
+    .then(setPokemon)
+  }, [])
 
+  console.log(pokemon)
   return (
     <div className="App">
-      <PokemonPage pokemon={pokemon} setPokemon={setPokemon} setSearchQuery={setSearchQuery}/>
+      <PokemonPage pokemon={pokemon} setPokemon={setPokemon}/>
     </div>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -4,22 +4,10 @@ import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
-function PokemonPage({pokemon, setPokemon, setSearchQuery}) {
-    // const [searchQuery, setSearchQuery] = useState("")
+function PokemonPage({pokemon, setPokemon}) {
+    const [searchQuery, setSearchQuery] = useState("")
     // console.log(searchQuery)
-
-    // const pokeDex = pokemon.filter((poke) => {
-    //   const name = poke.name
-    //   return (
-        
-    //     console.log(name.includes(searchQuery)
-    //   )
-    // console.log(searchQuery.includes(poke.name))
-    
-    // console.log(poke.name)
-    // poke.name.includes(searchQuery)
-    // }
-    // )
+    const pokeDex = pokemon.filter((poke) => poke.name.includes(searchQuery))
     
 
   return (
@@ -30,7 +18,7 @@ function PokemonPage({pokemon, setPokemon, setSearchQuery}) {
       <br />
       <Search setSearchQuery = {setSearchQuery}/>
       <br />
-      <PokemonCollection pokemon={pokemon}/>
+      <PokemonCollection pokemon={pokeDex}/>
     </Container>
   );
 }
